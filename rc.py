aac6ad6ba870b1dffb58c9c69a96e38f06254416
IoTtogawa <togawalab.proken.iot@gmail.com>
2019-06-12 12:13:48 +0900

    6/12

--- rc.py
+++ rc.py
@@ -10,61 +10,66 @@ LeftMotor =  [IN1, IN2]
 RightMotor = [IN3, IN4]
 
 def setup():
-	webiopi.debug("setup")
+	GPIO.setFunction(LeftMotor[0], GPIO.PWM)
+	GPIO.setFunction(LeftMotor[1], GPIO.PWM)
+	GPIO.setFunction(RightMotor[0], GPIO.PWM)
+	GPIO.setFunction(RightMotor[1], GPIO.PWM)
 
-	GPIO.setFunction(IN1, GPIO.OUT)
-	GPIO.setFunction(IN2, GPIO.OUT)
-	GPIO.setFunction(IN3, GPIO.OUT)
-	GPIO.setFunction(IN4, GPIO.OUT)
+
 
 def loop():
 	webiopi.sleep(0.1)
 
 def destroy():
-	GPIO.digitalWrite(IN1, GPIO.LOW)
-	GPIO.digitalWrite(IN2, GPIO.LOW)
-	GPIO.digitalWrite(IN3, GPIO.LOW)
-	GPIO.digitalWrite(IN4, GPIO.LOW)
+	GPIO.pwmWrite(LeftMotor[0], 0)
+	GPIO.pwmWrite(LeftMotor[1], 0)
+	GPIO.pwmWrite(RightMotor[0], 0)
+	GPIO.pwmWrite(RightMotor[1], 0)
 
 @webiopi.macro
 def stop():
-	GPIO.digitalWrite(LeftMotor[0], GPIO.LOW)
-	GPIO.digitalWrite(LeftMotor[1], GPIO.LOW)
-	GPIO.digitalWrite(RightMotor[0], GPIO.LOW)
-	GPIO.digitalWrite(RightMotor[1], GPIO.LOW)
+	GPIO.pwmWrite(LeftMotor[0], 0)
+	GPIO.pwmWrite(LeftMotor[1], 0)
+	GPIO.pwmWrite(RightMotor[0], 0)
+	GPIO.pwmWrite(RightMotor[1], 0)
 
 @webiopi.macro
 def forward():
-	#webiopi.debug("forward");
-
-	GPIO.digitalWrite(LeftMotor[0], GPIO.HIGH)
-	GPIO.digitalWrite(LeftMotor[1], GPIO.LOW)
-	GPIO.digitalWrite(RightMotor[0], GPIO.HIGH)
-	GPIO.digitalWrite(RightMotor[1], GPIO.LOW)
-
+	GPIO.pwmWrite(LeftMotor[0], 1)
+	GPIO.pwmWrite(LeftMotor[1], 0)
+	GPIO.pwmWrite(RightMotor[0], 1)
+	GPIO.pwmWrite(RightMotor[1], 0)
 @webiopi.macro
 def back():
-	#webiopi.debug("reverse");
-
-	GPIO.digitalWrite(LeftMotor[0], GPIO.LOW)
-	GPIO.digitalWrite(LeftMotor[1], GPIO.HIGH)
-	GPIO.digitalWrite(RightMotor[0], GPIO.LOW)
-	GPIO.digitalWrite(RightMotor[1], GPIO.HIGH)
+	GPIO.pwmWrite(LeftMotor[0], 0)
+	GPIO.pwmWrite(LeftMotor[1], 1)
+	GPIO.pwmWrite(RightMotor[0], 0)
+	GPIO.pwmWrite(RightMotor[1], 1)
 
 @webiopi.macro
-def left():
-	GPIO.digitalWrite(LeftMotor[0], GPIO.LOW)
-	GPIO.digitalWrite(LeftMotor[1], GPIO.HIGH)
-	GPIO.digitalWrite(RightMotor[0], GPIO.HIGH)
-	GPIO.digitalWrite(RightMotor[1], GPIO.LOW)
+def leftTurn():
+	GPIO.pwmWrite(LeftMotor[0], 0.5)
+	GPIO.pwmWrite(LeftMotor[1], 0)
+	GPIO.pwmWrite(RightMotor[0], 1)
+	GPIO.pwmWrite(RightMotor[1], 0)
 
 @webiopi.macro
-def right():
-	GPIO.digitalWrite(LeftMotor[0], GPIO.HIGH)
-	GPIO.digitalWrite(LeftMotor[1], GPIO.LOW)
-	GPIO.digitalWrite(RightMotor[0], GPIO.LOW)
-	GPIO.digitalWrite(RightMotor[1], GPIO.HIGH)
+def leftRoll():
+	GPIO.pwmWrite(LeftMotor[0], 0)
+	GPIO.pwmWrite(LeftMotor[1], 1)
+	GPIO.pwmWrite(RightMotor[0], 1)
+	GPIO.pwmWrite(RightMotor[1], 0)
 
+@webiopi.macro
+def rightTurn():
+	GPIO.pwmWrite(LeftMotor[0], 1)
+	GPIO.pwmWrite(LeftMotor[1], 0)
+	GPIO.pwmWrite(RightMotor[0], 0.5)
+	GPIO.pwmWrite(RightMotor[1], 0)
 
-
-
+@webiopi.macro
+def rightRoll():
+	GPIO.pwmWrite(LeftMotor[0], 1)
+	GPIO.pwmWrite(LeftMotor[1], 0)
+	GPIO.pwmWrite(RightMotor[0], 0)
+	GPIO.pwmWrite(RightMotor[1], 1)
